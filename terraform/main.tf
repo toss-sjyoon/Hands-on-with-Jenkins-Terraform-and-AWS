480b24639f2b6dcc5f28d8eb2a55c18ca00debbd
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-21 13:28:55 +0000

    fix: getting the ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 module "dpg_novmeber_elb" {
   source                   = "./modules/elastic_load_balancer"
   security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet_ids.id
+  subnet_id                = data.aws_subnet_ids.id.ids
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 
 module "dpg_november_asg" {
   source                   = "./modules/autoscaling_group"
   security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet_ids.id
+  subnet_id                = data.aws_subnet_ids.id.ids
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
