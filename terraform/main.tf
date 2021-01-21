e39d1463c0947d02be237c51f8d4bef39e1a94b8
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-21 11:38:59 +0000

    fix: define the sec group correctly

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 module "dpg_novmeber_elb" {
   source                   = "./modules/elastic_load_balancer"
-  security_group_id        = data.aws_security_group.id
+  security_group_id        = data.aws_security_group.sg.id
   subnet_id                = data.aws_subnet_ids.id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 
 module "dpg_november_asg" {
   source                   = "./modules/autoscaling_group"
-  security_group_id        = data.aws_security_group.id
+  security_group_id        = data.aws_security_group.sg.id
   subnet_id                = data.aws_subnet_ids.id
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
