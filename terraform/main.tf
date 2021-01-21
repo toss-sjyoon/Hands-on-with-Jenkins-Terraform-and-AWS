e45c766c9343c8a87601486382a04455cbe2249b
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-21 13:34:56 +0000

    fix: changing to subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 module "dpg_novmeber_elb" {
   source                   = "./modules/elastic_load_balancer"
   security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet_ids.id.ids.0
+  subnet_id                = data.aws_subnet_id.sn.id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 
 module "dpg_november_asg" {
   source                   = "./modules/autoscaling_group"
   security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet_ids.id.ids.0
+  subnet_id                = data.aws_subnet.sn.id
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
@@ -17,7 +17,7 @@ data "aws_security_group" "sg" {
     "Purpose" = "Playground"
   }
 }
-data "aws_subnet_ids" "id"{
+data "aws_subnet" "sn"{
   vpc_id = data.aws_vpc.vpc.id
   tags = {
     Purpose = "Playground"
