97c5209a70d3bfec967a5b60ac97440077f8570c
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-21 11:22:51 +0000

    feat: changed to data for subnet and sec group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,25 @@
 module "dpg_novmeber_elb" {
   source                   = "./modules/elastic_load_balancer"
-  security_group_id        = var.security_group_id
-  subnet_id                = var.subnet_id
+  security_group_id        = data.aws_security_group.id
+  subnet_id                = data.aws_subnet_ids.id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 
 module "dpg_november_asg" {
   source                   = "./modules/autoscaling_group"
-  security_group_id        = var.security_group_id
-  subnet_id                = var.subnet_id
+  security_group_id        = data.aws_security_group.id
+  subnet_id                = data.aws_subnet_ids.id
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
+}
+data "aws_security_group" "sg" {
+  tags = {
+    "Purpose" = "Playground"
+  }
+}
+data "aws_subnet_ids" "id"{
+  tags = {
+    Purpose = "Playground"
+    Tier = "private"
+  }
 }
