a83fba739550a30f44662483a92cfefdd23283dd
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-22 15:16:20 +0000

    feat: adding a destroy pipeleine

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -28,3 +28,33 @@ pipelineJob("Deploy-React-App"){
     }
   }
 }
+pipelineJob("Destroy-React-App"){
+    description("Destroys a React web application to AWS")
+    logRotator {
+        daysToKeep(5)
+        numToKeep(20)
+    }
+    concurrentBuild(allowConcurrentBuild = false)
+    triggers {
+        scm("* * * * *"){
+            ignorePostCommitHooks(ignorePostCommitHooks = false)
+        }
+    }
+    parameters {
+      stringParam("UNIQUE_ANIMAL_IDENTIFIER", defaultValue = "playground", description = "Your unique animal identifier for this playground!")
+    }
+    definition {
+    cpsScm {
+      scm {
+        git {
+          branch("master")
+          remote {
+            credentials("${GIT_USER}")
+            url("${GIT_URL}")
+          }
+        }
+      }
+      scriptPath('destroy.Jenkinsfile')
+    }
+  }
+}
